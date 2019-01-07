1f6d88f1bfcdf31eaa407323df3a8bf893d02565
pochammalapraveen <46436037+pochammalapraveen@users.noreply.github.com>
2019-01-07 13:21:38 +0530

    Update College.java

--- src/College.java
+++ src/College.java
@@ -14,36 +14,26 @@ import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 class College {
 
-    int j;
     String collegename;
     List<Department> departments = new ArrayList<>();
-    List<Student> Students = new ArrayList<>();
 
     public List<Department> createDepartment() throws IOException, ParseException {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 3; i++) {
+            Department department = new Department();
+            System.out.println("Enter Department");
             while (true) {
-                Department department = new Department();
-                System.out.println("enter Department");
-                while (true) {
-                    department.departmentname = in.readLine();
-                    if (department.departmentname.length() <= 50) {
-                        departments.add(department);
-                        Students = department.createStudent();
-                        j = 0;
-                        break;
-                    } else {
-                        System.out.println("Enter valid department");
-                    }
-                    if (j == 0) {
-                        break;
-                    }
+                department.departmentname = in.readLine();
+                if (department.departmentname.length() <= 50) {
+                    departments.add(department);
+                    department.createStudent();
+                    break;
+                } else {
+                    System.out.println("Enter valid department");
                 }
             }
         }
@@ -51,15 +41,14 @@ class College {
     }
 
     public static void main(String as[]) throws IOException, ParseException {
-        List<Department> department1 = new ArrayList<>();
         College college = new College();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("enter College name");
         while (true) {
             college.collegename = in.readLine();
-
             if (college.collegename.length() <= 50) {
-                department1 = college.createDepartment();
+                Department department = new Department();
+                college.createDepartment();
                 break;
             } else {
                 System.out.println("enter valid Collegename:");
